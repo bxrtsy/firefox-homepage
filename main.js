0afc46e790614b942d6ca355bf94323640d8efec
bxrtsy <124367691+bxrtsy@users.noreply.github.com>
2026-04-26 16:22:49 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// Add your shortcuts here
+const shortcuts = [
+	'www.wikipedia.org',
+	'github.com',
+	'www.figma.com',
+	'www.reddit.com',
+	'www.youtube.com',
+	'music.youtube.com',
+	'gemini.google.com',
+	// 'www.spotify.com'
+];
+
+const container = document.getElementById('shortcut-container');
+
+// Put SVG icon in shortcuts/ when adding a shortcut
+const content = shortcuts.map(url => `
+	<a href="https://${url}">
+		<img class="shortcut" src="shortcuts/${url}.svg">
+	</a>
+`);
+
+// content.push('<a href="main.js"><img class="shortcut" src="shortcuts/new.svg"></a>');
+
+container.innerHTML = content.join('');
